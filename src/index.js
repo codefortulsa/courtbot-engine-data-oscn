b001e3b87848fa24e035039b4dab5312be8756ad
Philip Kin <philip@thekinfamily.com>
2017-01-16 16:34:47 -0600

    made oscn api url a parameter

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import log4js from 'log4js';
 const client = new Client();
 const log = log4js.getLogger("oscn");
 
-export default function(county) {
+export default function(county, oscnApiUrl) {
   events.on("retrieve-parties", (casenumber, result) => {
+    const url = `${oscnApiUrl}/case/${county}/${casenumber}`;
+    log.debug(`Attempting to retrieve parties for casenumber ${casenumber}`);
+    log.debug(`using url: ${url}`)
     result.promises.push(new Promise(function(resolve, reject) {
-      client.get(`http://data.thekinfamily.com/oscn/case/${county}/${casenumber}`, function(data, res) {
+      client.get(url, function(data, res) {
         if(!data.defendants || data.defendants.length == 0) {
           log.info(`No defendants found in ${county} county for case number ${casenumber}`);
           resolve([]);
@@ -20,7 +23,7 @@ export default function(county) {
 
   events.on("retrieve-party-events", (casenumber, party, result) => {
     result.promises.push(new Promise(function(resolve, reject) {
-      client.get("http://data.thekinfamily.com/oscn/case/tulsa/" + casenumber + "/" + partyName, function(data, res) {
+      client.get(`${oscnApiUrl}/case/${county}/${casenumber}/${partyName}`, function(data, res) {
         console.dir(data);
         if(data.length != 1 || !data[0].events) {
           log.info(`No events found in ${county} county for case number ${casenumber} and party ${party}`);
